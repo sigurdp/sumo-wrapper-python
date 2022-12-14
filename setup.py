82026388e7551d0a46af4c96fa269e0fb4d49786
Per Olav Eide Svendsen <35033325+perolavsvendsen@users.noreply.github.com>
2022-12-14 19:42:27 +0100

    Loosen requirements and improve setup scripts (#119)
    * loosen requirements
    
    * update setup script
    
    * update CI
    
    * pytest access_token as env var
    
    Co-authored-by: Runar Ask Johannessen <ruaj@equinor.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,37 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements as parse
+from urllib.parse import urlparse
+
+
+def _format_requirement(req):
+    print(req)
+    if req.is_editable:
+        # parse out egg=... fragment from VCS URL
+        parsed = urlparse(req.requirement)
+        egg_name = parsed.fragment.partition("egg=")[-1]
+        without_fragment = parsed._replace(fragment="").geturl()
+        return f"{egg_name} @ {without_fragment}"
+    return req.requirement
+
+
+def parse_requirements(fname):
+    """Turn requirements.txt into a list"""
+    reqs = parse(fname, session="test")
+    return [_format_requirement(ir) for ir in reqs]
+
+
+REQUIREMENTS = parse_requirements("requirements/requirements.txt")
+REQUIREMENTS_DOCS = parse_requirements("requirements/requirements_docs.txt")
+
+EXTRAS_REQUIRE = {"docs": REQUIREMENTS_DOCS}
+
 
 setup(
     name="sumo-wrapper-python",
     description="Python wrapper for the Sumo API",
+    license="Apache 2.0",
     url="https://github.com/equinor/sumo-wrapper-python",
     keywords="sumo, python",
     classifiers=[
@@ -15,11 +42,12 @@ setup(
     ],
     use_scm_version={"write_to": "src/sumo/wrapper/version.py"},
     author="Equinor ASA",
-    install_requires=["requests", "msal", "PyYAML", "setuptools"],
+    install_requires=REQUIREMENTS,
     python_requires=">=3.4",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
+    extras_require=EXTRAS_REQUIRE,
     entry_points={"console_scripts": ["sumo_login = sumo.wrapper.login:main"]},
 )
