2a7abbdf1e9a22f4531cfb5134f6f4d11f02cef1
Roy Willy Haug <5484176+roywilly@users.noreply.github.com>
2023-09-25 13:32:30 +0200

    Never ever ever fail on logging (#148)

--- src/sumo/wrapper/_logging.py
+++ src/sumo/wrapper/_logging.py
@@ -9,17 +9,17 @@ class LogHandlerSumo(logging.Handler):
         return
 
     def emit(self, record):
-        dt = datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
-        json = {
-            "severity": record.levelname,
-            "message": record.getMessage(),
-            "timestamp": dt,
-            "source": record.name,
-            "pathname": record.pathname,
-            "funcname": record.funcName,
-            "linenumber": record.lineno,
-        }
         try:
+            dt = datetime.utcnow().replace(microsecond=0).isoformat() + "Z"
+            json = {
+                "severity": record.levelname,
+                "message": record.getMessage(),
+                "timestamp": dt,
+                "source": record.name,
+                "pathname": record.pathname,
+                "funcname": record.funcName,
+                "linenumber": record.lineno,
+            }
             self._sumoClient.post("/message-log/new", json=json)
         except Exception:
             # Never fail on logging
